b9d840b0a7d497fe400d8aae1f27a3bc728e25ad
dideco1 <diegobaro.si@outlook.com>
2022-11-16 15:53:16 -0300

    Projeto finalizado

--- CRUD com SQLite/CRUD com SQLite/App.xaml.cs
+++ CRUD com SQLite/CRUD com SQLite/App.xaml.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using CRUD_com_SQLite.Helper;
+using System.IO;
 
 namespace CRUD_com_SQLite
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper database;
+        public static SQLiteDatabaseHelper Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "arquivo.db3");
+                    database = new SQLiteDatabaseHelper(path);
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new View.Listagem());
         }
 
         protected override void OnStart()
--- CRUD com SQLite/CRUD com SQLite/Helper/SQLiteDatabaseHelper.cs
+++ CRUD com SQLite/CRUD com SQLite/Helper/SQLiteDatabaseHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks
 using CRUD_com_SQLite.Model;
 using SQLite;
 
@@ -8,25 +9,35 @@ namespace CRUD_com_SQLite.Helper
 {
     public class SQLiteDatabaseHelper
     {
-        public void insert(produto p)
-        {
+        readonly SQLiteAsyncConnection _conn;
 
-        }
-        public void update(produto p)
+        public SQLiteDatabaseHelper(string path)
         {
-
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<produto>().Wait();
         }
-        public produto getById(int id)
+        public Task<int> insert(produto p)
         {
-            return new produto();
+            return _conn.InsertAsync(p);
         }
-        public void getAll()
+        public Task<List<void>> update(produto p)
         {
-
+            string sql = "UPDATE produto SET descricao=?, quantidade=?, preco=? WHERE id=? ";
+            return _conn.QueryAsync<produto>(sql, p.descricao, p.quantidade, p.preco, p.id);
         }
-        public void delete()
+        
+        public Task<List<produto>> getAll(string q)
         {
-
+            return _conn.Table<produto>().ToListAsync();
+        }
+        public Task<int> delete(int id)
+        {
+            _conn.Table<produto>().DeleteAsync(i => i.id == id);
+        }
+        public Task<List<produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM produto WHERE descricao LIKE '%" + q + "%' ";
+            _conn.QueryAsync<produto>(sql);
         }
     }
 }
