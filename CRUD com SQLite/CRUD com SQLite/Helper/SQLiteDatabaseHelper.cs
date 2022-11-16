de6aa12865431058826e02d4a765c609dae9f47a
dideco1 <diegobaro.si@outlook.com>
2022-11-16 14:26:22 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD com SQLite/CRUD com SQLite/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CRUD_com_SQLite.Model;
+using SQLite;
+
+namespace CRUD_com_SQLite.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        public void insert(produto p)
+        {
+
+        }
+        public void update(produto p)
+        {
+
+        }
+        public produto getById(int id)
+        {
+            return new produto();
+        }
+        public void getAll()
+        {
+
+        }
+        public void delete()
+        {
+
+        }
+    }
+}
